5091ea3ac86df6547fc5d5e44bd40b28e292d67e
Kadsonklein <kad200178@yahoo.com.br>
2025-04-08 15:38:19 -0300

    Inicial

---
+++ main.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+
+class HomePage(Screen):
+    pass
+
+class Config(Screen):
+    pass
+
+GUI = Builder.load_file("main.kv")
+class MainApp(App):
+    def build(self):
+        return GUI
+    def on_start(self):
+        pass
+
+    def mudar_tela(self, id_tela):
+        gerenciador_telas = self.root.ids["screen_manager"]
+        gerenciador_telas.current = id_tela
+
+MainApp().run()
